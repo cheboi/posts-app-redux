72a39a6dd3079e3fd2957050e5d77a720c0c128b
cheboi <moscheb96@gmail.com>
2022-11-09 10:03:33 +0300

    implemented likes

--- src/redux/reducers/postReducer.js
+++ src/redux/reducers/postReducer.js
@@ -1,24 +1,43 @@
-import { CREATE_POST_ACTION, DELETE_POST_ACTION } from "../types";
+import {
+  CREATE_POST_ACTION,
+  DELETE_POST_ACTION,
+  LIKE_POST_ACTION,
+} from "../types";
 
 const initialState = {
-	posts: [],
-	name:"Awesome App"
+  posts: [],
+  name: "Awesome App",
+  likes: 0,
 };
 
 export const postReducer = (state = initialState, action) => {
-    // if (action.type === CREATE_POST_ACTION) {
-    //     return { ...state, posts: [...state.posts, action.post] };
-    // } else {
-    // 	 return state
-    // }
+  // if (action.type === CREATE_POST_ACTION) {
+  //     return { ...state, posts: [...state.posts, action.post] };
+  // } else {
+  // 	 return state
+  // }
 
-    switch (action.type) {
-        case CREATE_POST_ACTION:
-			return { ...state, posts: [...state.posts, action.post] };
-		case DELETE_POST_ACTION:
-			const newPosts = state.posts.filter(post => post.id !== action.id);
-			return { ...state, posts: newPosts };
-        default:
-            return state;
-    }
+  switch (action.type) {
+    case CREATE_POST_ACTION:
+      return { ...state, posts: [...state.posts, action.post] };
+    case DELETE_POST_ACTION:
+      const newPosts = state.posts.filter((post) => post.id !== action.id);
+      return { ...state, posts: newPosts };
+    case LIKE_POST_ACTION:
+      return {
+        ...state,
+        posts: state.posts.map((post) => {
+          if (post.id === action.id) {
+            return {
+              ...post,
+              likes: post.likes + 1,
+            };
+          }
+          return post;
+        }),
+        likes: state.likes + 1
+      };
+    default:
+      return state;
+  }
 };
